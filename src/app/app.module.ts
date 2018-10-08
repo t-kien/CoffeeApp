6e2b846fdb468553daa0d1c0ed8617d55a3b94eb
WPG Team <support@wpgroupbuy.com>
2018-10-08 11:19:20 +0700

    HomeApp Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoffeeComponent } from '../coffee/coffee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CoffeeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CoffeeComponent]
 })
 export class AppModule { }
---
+++ src/coffee/coffee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coffee-root',
+  templateUrl: './coffee.component.html',
+  styleUrls: ['./coffee.component.css'], // mảng nên có []
+})
+export class CoffeeComponent {
+    
+}
